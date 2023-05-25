413b32eacb37be001d331e87822dea1bd3738e7e
Toitles123 <jiangjeric@gmail.com>
2023-05-24 22:20:41 -0400

    Push

---
+++ Rover Simulation/Assets/IKTest.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IKTest : MonoBehaviour
+{
+    public Transform origin;
+    public Transform joint;
+    public Transform endJoint;
+    public Transform target;
+
+    public float linkLength1 = 0.75f;
+    public float linkLength2 = 0.25f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector3 translatedPosition = new Vector3(new Vector2(target.position.x, target.position.z).magnitude, target.position.y);
+        float zRot = Mathf.Atan2(target.position.z - origin.position.z, target.position.x - origin.position.x);
+        float dist = Vector3.Distance(translatedPosition, origin.position);
+        
+    }
+}
--- Rover Simulation/Assets/RoverScript.cs
+++ Rover Simulation/Assets/RoverScript.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class RoverScript : MonoBehaviour
 {
     public HingeJoint[] wheels;
+    public float maxSpeed;
+    float currForwardSpeed;
+    float currBackwardSpeed;
 
     void Update()
     {
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
-        float wheelPowerLF = input.y - input.x;
-        float wheelPowerLR = input.y - input.x;
-        float wheelPowerRF = input.y + input.x;
-        float wheelPowerRR = input.y + input.x;
+        float wheelPowerLF = -input.y - input.x;
+        float wheelPowerLR = -input.y - input.x;
+        float wheelPowerRF = -input.y + input.x;
+        float wheelPowerRR = -input.y + input.x;
 
         float max = Mathf.Max(new float[4] { Mathf.Abs(wheelPowerLF), Mathf.Abs(wheelPowerLR), Mathf.Abs(wheelPowerRF), Mathf.Abs(wheelPowerRR) });
         if (max > 1)
@@ -24,10 +27,20 @@ public class RoverScript : MonoBehaviour
             wheelPowerRR /= Mathf.Abs(max);
         }
 
-        wheelPowerLF *= 500;
-        wheelPowerLR *= 500;
-        wheelPowerRF *= 500;
-        wheelPowerRR *= 500;
+        if (input.x > 0 || -input.y > 0)
+            currForwardSpeed = Mathf.Lerp(currForwardSpeed, maxSpeed, 0.1f);
+        else
+            currForwardSpeed = Mathf.Lerp(currForwardSpeed, 0, 0.1f);
+        
+        if (input.x < 0 || -input.y < 0)
+            currBackwardSpeed = Mathf.Lerp(currBackwardSpeed, maxSpeed, 0.1f);
+        else
+            currBackwardSpeed = Mathf.Lerp(currBackwardSpeed, 0, 0.1f);
+
+        wheelPowerLF *= wheelPowerLF > 0 ? currForwardSpeed : currBackwardSpeed;
+        wheelPowerLR *= wheelPowerLR > 0 ? currForwardSpeed : currBackwardSpeed;
+        wheelPowerRF *= wheelPowerRF > 0 ? currForwardSpeed : currBackwardSpeed;
+        wheelPowerRR *= wheelPowerRR > 0 ? currForwardSpeed : currBackwardSpeed;
 
         wheels[0].motor = ChangeTargetVelocity(wheels[0], wheelPowerLF);
         wheels[1].motor = ChangeTargetVelocity(wheels[1], wheelPowerLR);
